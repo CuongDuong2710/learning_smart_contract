3e406577987c560161eb93ac1f061c744ed2030a
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-03-28 10:20:45 +0700

    add unit test for SampleToken

---
+++ first_project/test/test-token.js
@@ -0,0 +1,53 @@
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+
+describe("ERC-20 BEP-20 sample token", function() {
+    let [accountA, accountB, accountC] = []
+    let token
+    let amount = 100
+    let totalSupply = 1000000
+    beforeEach(async () => {
+        [accountA, accountB, accountC] = await ethers.getSigners()
+        const Token = ethers.getContractFactory("SampleToken")
+        token = await Token.deploy()
+        await token.deployed()
+    })
+    describe("common", function() {
+        it("total supply should return right value", async function(){
+
+        })
+        it("balance of account A should return right value", async function(){
+            
+        })
+        it("balance of account B should return right value", async function(){
+            
+        })
+        it("allowance of account A to account B should return right value", async function(){
+            
+        })
+    })
+    describe("transfer", function() {
+        it("transfer should revert if amount exceeds balance", async function(){
+            
+        })
+        it("transfer should work correctly", async function(){
+            
+        })
+    })
+    describe("transferFrom", function() {
+        it("transferFrom should revert if amount exceeds balance", async function(){
+            
+        })
+        it("transferFrom should revert if amount exceeds allowance amount", async function(){
+            
+        })
+        it("transferFrom should work correctly", async function(){
+            
+        })
+    })
+    describe("approve", function() {
+        it("approve should work correctly", async function(){
+            
+        })
+    })
+})
