9d85dc85904d146174e107e4bd8987e50a3a231d
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-03-30 11:21:32 +0700

    add baseURI private

--- first_project/contracts/nft/petty.sol
+++ first_project/contracts/nft/petty.sol
@@ -7,15 +7,24 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Petty is ERC721, Ownable {
     using Counters for Counters.Counter;
-
+    string private _baseTokenURI;
     Counters.Counter private _tokenIdCount;
 
     constructor() ERC721("Petty", "PET") {}
 
+    // only contract owner has permission to call mint() 
     function mint(address to_) public onlyOwner returns (uint256) {
         _tokenIdCount.increment();
         uint256 tokenId = _tokenIdCount.current();
         _mint(to_, tokenId);
         return tokenId;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function updateBaseURI(string memory baseTokenURI_) public onlyOwner {
+        _baseTokenURI = baseTokenURI_;
     }
 }
