1c4045afb92b2d011b26255dc2f022c096826e53
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-04-04 09:56:40 +0700

    add Counter, EnumrableSet, orders list and fee

--- first_project/contracts/marketplace/Marketplace.sol
+++ first_project/contracts/marketplace/Marketplace.sol
@@ -4,8 +4,13 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 contract Marketplace is Ownable {
+    using Counters for Counters.Counter;
+    using EnumerableSet for EnumerableSet.AddressSet;
+
     struct Order {
         address seller;
         address buyer;
@@ -13,4 +18,11 @@ contract Marketplace is Ownable {
         address paymentToken;
         uint256 price;
     }
+    Counters.Counter private _orderIdCount;
+    IERC721 public immutable nftContracts; // read-only and create one time in constructor
+    mapping(uint256 => Order) orders;
+    uint256 feeDecimal;
+    uint256 feeRate;
+    address public feeRecipient; // address receives fee of transaction
+    EnumerableSet.AddressSet private _supportedPaymentTokens;
 }
