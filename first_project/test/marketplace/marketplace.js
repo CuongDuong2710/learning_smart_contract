d8f853acac93b21e413c8780f53060fb4e6e27e4
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-04-14 10:49:43 +0700

    add common describe

--- first_project/test/marketplace/marketplace.js
+++ first_project/test/marketplace/marketplace.js
@@ -12,24 +12,43 @@ describe("marketplace", function () {
   let defaultBalance = ethers.utils.parseEther("1000");
   let address0 = "0x0000000000000000000000000000000000000000";
   beforeEach(async () => {
-    [admin, seller, buyer, feeRecipient, samplePaymentToken] = await ethers.getSigners()
+    [admin, seller, buyer, feeRecipient, samplePaymentToken] =
+      await ethers.getSigners();
 
     // deployed Petty contract
-    const Petty = await ethers.getContractFactory("Petty")
-    petty = await Petty.deploy()
-    await petty.deployed()
+    const Petty = await ethers.getContractFactory("Petty");
+    petty = await Petty.deploy();
+    await petty.deployed();
 
     // deployed Gold contract
-    const Gold = await ethers.getContractFactory("Gold")
-    gold = await Gold.deploy()
-    await gold.deployed()
+    const Gold = await ethers.getContractFactory("Gold");
+    gold = await Gold.deploy();
+    await gold.deployed();
 
     // deployed Marketplace contract
-    const Marketplace = await ethers.getContractFactory("Marketplace")
-    marketplace = await Marketplace.deploy(petty.address, defaultFeeDecimal, defaultFeeRate, feeRecipient.address)
-    await marketplace.deployed()
-    await marketplace.addPaymentToken(gold.address) // add payment token
-    await gold.transfer(seller, defaultBalance)
-    await gold.transfer(buyer, defaultBalance)
+    const Marketplace = await ethers.getContractFactory("Marketplace");
+    marketplace = await Marketplace.deploy(
+      petty.address,
+      defaultFeeDecimal,
+      defaultFeeRate,
+      feeRecipient.address
+    );
+    await marketplace.deployed();
+    await marketplace.addPaymentToken(gold.address); // add payment token
+    await gold.transfer(seller, defaultBalance);
+    await gold.transfer(buyer, defaultBalance);
+  });
+  describe("common", function () {
+    it("feeDecimal should return correct value", async function () {
+      expect(await marketplace.feeDecimal()).to.be.equal(defaulFeeDecimal);
+    });
+    it("feeRate should return correct value", async function () {
+      expect(await marketplace.feeRate()).to.be.equal(defaultFeeRate);
+    });
+    it("feeRecipient should return correct value", async function () {
+      expect(await marketplace.feeRecipient()).to.be.equal(
+        feeRecipient.address
+      );
+    });
   });
 });
