4d2200569434d78ee641128ea75493d5992bb112
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-04-18 10:32:19 +0700

    add unit test for Reserve contract

---
+++ first_project/test/reserve/reserve.js
@@ -0,0 +1,20 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Reserve", function () {
+  let [admin, receiver, seller, buyer] = [];
+  let gold;
+  let reserve;
+  let address0 = "0x0000000000000000000000000000000000000000";
+  let reserveBalance = ethers.utils.parseEther("1000");
+  let oneWeek = 86400 * 7;
+  beforeEach(async () => {
+    [admin, receiver, seller, buyer] = await ethers.getSigners();
+    const Gold = await ethers.getContractFactory("Gold");
+    gold = await Gold.deploy();
+    await gold.deployed();
+    const Reserve = await ethers.getContractFactory("Reserve");
+    reserve = await Reserve.deploy();
+    await reserve.deployed(gold.address);
+  });
+});
