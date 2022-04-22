1b5a7a4e58fa2ff7bb9a967be3b869ad0d064f00
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-04-22 10:20:44 +0700

    create CryptoDevs contract

---
+++ nft-collection-learnweb3DAO/harthat-tutorial/contracts/CryptoDevs.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IWhitelist.sol";
+
+contract CryptoDevs is ERC721Enumerable, Ownable {
+    /**
+     * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`.
+     */
+    string _baseTokenURI;
+
+    //  _price is the price of one Crypto Dev NFT
+    uint256 public _price = 0.01 ether;
+
+    // _paused is used to pause the contract in case of an emergency
+    bool public _paused;
+
+    // max number of CryptoDevs
+    uint256 public maxTokenIds = 20;
+
+    // total number of tokenIds minted
+    uint256 public tokenIds;
+
+    // Whitelist contract instance
+    IWhitelist whitelist;
+
+    // boolean to keep track of whether presale started or not
+    bool public presaleStarted;
+
+    // timestamp for when presale would end
+    uint256 public presaleEnded;
+
+    modifier onlyWhenNotPaused() {
+        require(!_pause, "Contract currently paused");
+        _;
+    }
+
+    /**
+     * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
+     * name in our case is `Crypto Devs` and symbol is `CD`.
+     * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
+     * It also initializes an instance of whitelist interface.
+     */
+    constructor(string memory baseURI, address whitelistContract)
+        ERC721("Crypto Devs", "CD")
+    {
+        _baseTokenURI = baseURI;
+        whitelist = IWhitelist(whitelistContract);
+    }
+
+    /**
+     * @dev startPresale starts a presale for the whitelisted addresses
+     */
+    function startPresale() public onlyOwner {
+        presaleStarted = true;
+        presaleEnded = block.timestamp + 5 minutes;
+    }
+}
