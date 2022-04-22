a0f42d3457de4cbbe3c00e234c209418212314be
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-04-22 13:01:00 +0700

    install dotenv

---
+++ nft-collection-learnweb3DAO/harthat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x596546ec0aeAd952CFca68B54Fc184e945082C95";
+// URL for the Crypto Devs NFT Metadata
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }
