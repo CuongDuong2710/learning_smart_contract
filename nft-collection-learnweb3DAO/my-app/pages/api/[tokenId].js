b21cc3327461f6916607218f3d87167275fa3b94
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-04-28 12:43:27 +0700

    add [tokenId].js api

---
+++ nft-collection-learnweb3DAO/my-app/pages/api/[tokenId].js
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  // get the tokenId from the query params
+  const tokenId = req.query.tokenId;
+  // As all the images are uploaded on github, we can extract the images from github directly.
+  // Click on Image and select Raw button
+  const imgUrl =
+    "https://raw.githubusercontent.com/CuongDuong2710/learning_smart_contract/main/nft-collection-learnweb3DAO/my-app/public/cryptodevs/";
+  // The api is sending back metadata for a Crypto Dev
+  // To make our collection compatible with Opensea, we need to follow some Metadata standards
+  // when sending back the response from the api
+  // More info can be found here: https://docs.opensea.io/docs/metadata-standards
+  res.status(200).json({
+    name: "Crypto Dev #" + tokenId,
+    description: "Crypto Dev is a collection of developers in crypto",
+    image: imgUrl + tokenId + ".svg",
+  });
+}
