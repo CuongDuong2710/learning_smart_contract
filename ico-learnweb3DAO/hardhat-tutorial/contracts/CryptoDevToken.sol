30be4eab9762ef300a9e6786a29b17f83a908b5a
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-05-09 10:17:14 +0700

    [ICO-LearnWeb3] create CryptoDevToken contract

---
+++ ico-learnweb3DAO/hardhat-tutorial/contracts/CryptoDevToken.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ICryptoDevs.sol";
+
+contract CryptoDevToken is ERC20, Ownable {
+    // Price of one Crypto Dev token
+    uint256 public constant tokenPrice = 0.001 ether;
+
+    // Each NFT would give the user 10 tokens
+    // It needs to be represented as 10 * (10 ** 18) as ERC20 tokens are represented by the smallest denomination possible for the token
+    // By default, ERC20 tokens have the smallest denomination of 10^(-18). This means, having a balance of (1)
+    // is actually equal to (10 ^ -18) tokens.
+    // Owning 1 full token is equivalent to owning (10^18) tokens when you account for the decimal places.
+    // More information on this can be found in the Freshman Track Cryptocurrency tutorial.
+    uint256 public constant tokensPerNFT = 10 * 10**18;
+
+    // the max total supply is 10000 for Crypto Dev Tokens
+    uint256 public constant maxTotalSupply = 10000 * 10**18;
+
+    // CryptoDevsNFT contract instance
+    ICryptoDevs CryptoDevsNFT;
+
+    // Mapping to keep track of which tokenIds have been claimed
+    mapping(uint256 => bool) public tokenIdsClaimed;
+
+    constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
+        CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
+    }
+
+    /**
+     * @dev Mints `amount` number of CryptoDevTokens
+     * Requirements:
+     * - `msg.value` should be equal or greater than the tokenPrice * amount
+     */
+    function mint(uint256 amount) public payable {
+        // the value of ether that should be equal or greater than tokenPrice * amount;
+        uint256 _requiredAmount = tokenPrice * amount;
+        require(msg.value >= _requiredAmount);
+
+        // total tokens + amount <= 10000, otherwise revert the transaction
+        uint256 amountWithDecimals = amount * 10**18;
+        require(
+            (totalSupply() + amountWithDecimals) <= maxTotalSupply,
+            "Exceeds the max total supply available."
+        );
+        _mint(msg.sender, amountWithDecimals);
+    }
+}
