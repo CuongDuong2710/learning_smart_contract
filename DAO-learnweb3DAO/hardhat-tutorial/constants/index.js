528a21f9ac04eaa39fb8d7101cf743d0276d5f3a
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-05-31 10:08:55 +0700

    [DAO-LearnWeb3] create deploy, constants and config file

---
+++ DAO-learnweb3DAO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x6bc126de61105Dd2C675dd16025cF9fd020cd0b6";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
