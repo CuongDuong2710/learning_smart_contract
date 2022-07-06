44a10232b209447331728d230a6be1c039f87c8b
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-07-06 15:06:00 +0700

    [LearnWeb3DAO] junior track - Etherscan verification

--- junior_track_LearnWeb3DAO/hardhat-verification/README.md
+++ junior_track_LearnWeb3DAO/hardhat-verification/README.md
@@ -13,3 +13,15 @@ npx hardhat node
 node scripts/sample-script.js
 npx hardhat help
 ```
+
+# compile error: Error HH502: Couldn't download compiler versions list. Please check your connection. #2710 (when run "npx hardhat compile")
+npx hardhat --show-stack-traces --verbose compile
+https://github.com/NomicFoundation/hardhat/pull/1291
+
+# Error: self signed certificate in certificate chain
+https://stackoverflow.com/questions/9626990/receiving-error-error-ssl-error-self-signed-cert-in-chain-while-using-npm
+https://stackoverflow.com/questions/45088006/nodejs-error-self-signed-certificate-in-certificate-chain
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+#
+Verify Contract Address: 0x0aa0cD89DEdb9A87c1f95bFc7ae1cF9F00005146
---
+++ junior_track_LearnWeb3DAO/hardhat-verification/scripts/deploy.js
@@ -0,0 +1,43 @@
+const { ethers } = require('hardhat')
+require('dotenv').config({ path: '.env' })
+require('@nomiclabs/hardhat-etherscan')
+const hre = require("hardhat");
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+async function main() {
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so verifyContract here is a factory for instances of our Verify contract.
+  */
+  const verifyContract = await ethers.getContractFactory('Verify')
+
+  // deploy the contract
+  const deployedVerifyContract = await verifyContract.deploy()
+
+  await deployedVerifyContract.deployed()
+
+  // print the address of the deployed contract
+  console.log('Verify Contract Address:', deployedVerifyContract.address)
+
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(10000)
+
+  // Verify the contract after deploying
+  await hre.run('verify:verify', {
+    address: deployedVerifyContract.address,
+    constructorArguments: [],
+  })
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
