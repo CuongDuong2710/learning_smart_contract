de7ddd2b1707baef30cef54f4580f37b987ead2d
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-07-14 10:06:41 +0700

    [LearnWeb3DAO] junior track - IPFS practical

--- junior_track_LearnWeb3DAO/ipfs_practical/my-app/pages/index.js
+++ junior_track_LearnWeb3DAO/ipfs_practical/my-app/pages/index.js
@@ -1,46 +1,81 @@
-import { Contract, providers, utils } from 'ethers'
-import Head from 'next/head'
-import React, { useEffect, useRef, useState } from 'react'
-import Web3Modal from 'web3modal'
-import { abi, NFT_CONTRACT_ADDRESS } from '../constants'
-import styles from '../styles/Home.module.css'
+import { Contract, providers, utils } from "ethers";
+import Head from "next/head";
+import React, { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
+import { abi, NFT_CONTRACT_ADDRESS } from "../constants";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   // walletConnected keep track of whether the user's wallet is connected or not
-  const [walletConnected, setWalletConnected] = useState(false)
+  const [walletConnected, setWalletConnected] = useState(false);
   // loading is set to true when we are waiting for a transaction to get mined
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   // tokenIdsMinted keeps track of the number of tokenIds that have been minted
-  const [tokenIdsMinted, setTokenIdsMinted] = useState('0')
+  const [tokenIdsMinted, setTokenIdsMinted] = useState("0");
   // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
-  const web3ModalRef = useRef()
+  const web3ModalRef = useRef();
 
   /**
    * publicMint: Mint an NFT
    */
   const publicMint = async () => {
     try {
-      console.log('Public mint')
+      console.log("Public mint");
       // We need a Signer here since this is a 'write' transaction.
-      const signer = await getProviderOrSigner()
+      const signer = await getProviderOrSigner();
       // Create a new instance of the Contract with a Signer, which allows
       // update methods
-      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer)
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
       // call the mint from the contract to mint the LW3Punks
       const tx = await nftContract.mint({
         // value signifies the cost of one LW3Punks which is "0.01" eth.
         // We are parsing `0.01` string to ether using the utils library from ethers.js
-        value: utils.parseEther('0.01'),
-      })
-      setLoading(true)
+        value: utils.parseEther("0.01"),
+      });
+      setLoading(true);
       // wait for the transaction to get mined
-      await tx.wait()
-      setLoading(false)
-      window.alert('You successfully minted a LW3Punk!')
+      await tx.wait();
+      setLoading(false);
+      window.alert("You successfully minted a LW3Punk!");
     } catch (error) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
-  return <div></div>
+  /*
+        connectWallet: Connects the MetaMask wallet
+      */
+  const connectWallet = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // When used for the first time, it prompts the user to connect their wallet
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  /**
+   * getTokenIdsMinted: gets the number of tokenIds that have been minted
+   */
+  const getTokenIdsMinted = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // No need for the Signer here, as we are only reading state from the blockchain
+      const provider = await getProviderOrSigner();
+      // We connect to the Contract using a Provider, so we will only
+      // have read-only access to the Contract
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+      // call the tokenIds from the contract
+      const _tokenIds = await nftContract.tokenIds();
+      console.log("tokenIds", _tokenIds);
+      //_tokenIds is a `Big Number`. We need to convert the Big Number to a string
+      setTokenIdsMinted(_tokenIds.toString());
+    } catch (error) {
+      console.error(err);
+    }
+  };
+
+  return <div></div>;
 }
