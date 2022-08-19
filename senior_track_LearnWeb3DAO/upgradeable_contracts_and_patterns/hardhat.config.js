10b166d53d01c00fd7d8582f588c0a53febdb83b
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2022-08-19 15:52:51 +0700

    [LearnWeb3DAO] senior track - Upgradeable Contracts and Patterns

--- senior_track_LearnWeb3DAO/upgradeable_contracts_and_patterns/hardhat.config.js
+++ senior_track_LearnWeb3DAO/upgradeable_contracts_and_patterns/hardhat.config.js
@@ -1,6 +1,7 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.4",
 };
